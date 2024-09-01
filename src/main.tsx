b32b9475d5da9b0cdf2c4a385972bb597d1de43a
Figo Ferdian <figoferdian18@gmail.com>
2024-09-01 12:03:39 +0700

    delete profile

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home.tsx';
-import Profile from './profile.tsx'; // Pastikan Profile diimpor dengan benar
+
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')!).render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/figo" element={<Profile />} />
+
       </Routes>
     </Router>
   </StrictMode>
